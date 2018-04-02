d29ef187c2107a4f349fe1d099633fd97114c71e
Yoga <yogasandyana92@gmail.com>
2018-04-02 10:42:02 +0800

    Header File

---
+++ application/views/v_header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Stasiun Geofisika Sanglah</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color:white">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="margin-top: 25px;">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <div class="navbar-brand" style="height: 80px">
+        <a href="#myPage">
+          <img src="<?php echo base_url('assets/img/logo.png') ?>" width="50px">
+          <span class="hidden-xs hidden-md">
+          </span>
+        </a>
+    </div>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right" style="font-size:11px; padding-top:15px">
+        <li><a href="#about" style="font-weight:bold; color:black">PROFIL</a></li>
+        <li><a href="#services" style="font-weight:bold; color:black">GEOFISIKA</a></li>
+        <li><a href="#portfolio" style="font-weight:bold; color:black">METEOROLGI</a></li>
+        <li><a href="#pricing" style="font-weight:bold; color:black">KLIMATOLOGI</a></li>
+        <li><a href="#contact"style="font-weight:bold; color:black">PUBLIKASI</a></li>
+        <li><a href="#contact" style="font-weight:bold; color:black">LAYANAN ONLINE</a></li>
+        <li><a href="#contact" style="font-weight:bold; color:black">ARTIKEL</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
