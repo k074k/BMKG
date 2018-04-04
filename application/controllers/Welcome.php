233027d5e786cfc0fc6ea0a541379abec3f495d3
Yoga <yogasandyana92@gmail.com>
2018-04-04 09:56:29 +0800

    Dashboard File & Assets

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,6 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('v_header');
+        $this->load->view('v_dashboard');
 		$this->load->view('v_footer');
 	}
 }
---
+++ application/views/v_dashboard.php
@@ -0,0 +1,34 @@
+<div class="container-fliud" style="background-color: #e6e6e659; height: 600px">
+    <div class="container">
+        <div class="row" style="margin-top: 80px;">
+            <div class="col-lg-4">
+                <ul class="nav nav-tabs" style="margin-top:25px">
+                    <li class="active">
+                        <a data-toggle="tab" href="#Gempa" style="font-size: 12px; font-weight: bold">Gempabumi M ≥ 5.0</a>
+                    </li>
+                    <li>
+                        <a data-toggle="tab" href="#Gempa2" style="font-size: 12px; font-weight: bold">Gempabumi Dirasakan</a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div id="Gempa" class="tab-pane fade in active">
+                        <div style="margin-top:10px; background-color:white; height: 300px; box-shadow: 1px 1px 3px grey;" class="col-lg-12">
+                        </div>
+                    </div>
+                    <div id="Gempa2" class="tab-pane fade">
+                        <div style="margin-top:10px; background-color:white; height: 300px; box-shadow: 1px 1px 3px grey;" class="col-lg-12">
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+            <div class="col-lg-8">
+                <ul class="nav nav-tabs" style="margin-top: 25px">
+                    <li class="active" style="font-size: 12px; font-weight: bold"><a>Peta Gempabumi</a></li>
+                </ul>
+                <div style="margin-top:10px; background-color:white; height: 400px; box-shadow: 1px 1px 3px grey;" class="col-lg-12">
+            </div>
+        </div>
+    </div>
+</div>
