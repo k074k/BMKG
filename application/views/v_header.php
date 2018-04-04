ada8c1e6389ebd1277bce2f6963cc61235f3ade1
Yoga <yogasandyana92@gmail.com>
2018-04-04 17:37:13 +0800

    Fix CSS

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -7,9 +7,9 @@
         <title>Stasiun Geofisika Sanglah</title>  
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
         <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
-        <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url('assets/js/java.js'); ?>"></script>
  
           <!-- CSS --> 
           <style> 
