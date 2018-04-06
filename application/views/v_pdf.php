142588393201bd65773c0ed73148e31c91aca1f8
Nusan Panca Sudarma <pancanusan@gmail.com>
2018-04-06 13:20:40 +0800

    Script Pdf dengan file.pdf

---
+++ application/views/v_pdf.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="container"  style="margin-bottom: 80px; margin-top: 70px">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="<?php echo base_url('assets/WELCOME.pdf'); ?>" allowfullscreen></iframe>
+      </div>
+    </div>
+</div>
