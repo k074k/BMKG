e28f1fba2a7c89db9625f70d75baa554fef61f64
Yoga <yogasandyana92@gmail.com>
2018-04-06 12:44:06 +0800

    Revisi Menu Publikasi

--- application/views/v_publikasi.php
+++ application/views/v_publikasi.php
@@ -2,11 +2,12 @@
     <div class="container" style="margin-left:45px;">
         <div class="row">
             <h3>Publikasi</h3>
+            <hr style="box-shadow: 0px 1px 1px grey;">
         </div>
     </div>
 </div>
-<div class="col-lg-12" style="background-color: white; margin-top: 25px; box-shadow: 1px 1px 1px;">
-    <div class="col-lg-8" style="margin-bottom: 65px; padding-left: 10%; padding-top: 50px; ">
+<div class="col-lg-12">
+    <div class="col-lg-8" style="margin-bottom: 65px; padding-left: 10%; padding-top: 1%; ">
         <div class="row">
             <div class="col-lg-3">
                 <form role="form">
@@ -35,13 +36,49 @@
                 </form>
             </div>
         </div>
-        <div class="row">
-            
+        <div class="row" style="padding-left: 2%; padding-right: 2%; ">
+            <div class="col-lg-12" style="background-color: white; box-shadow: 1px 1px 3px grey; padding-top: 1%">
+                <h2 style="font-weight: bold">
+                    GEODINAMIKA
+                </h2>
+                <p style="font-size: 12px">
+                    Nomer Katalog : - | Nomer Publikasi : - | Tanggal Rilis : - | Ukuran : -
+                </p><hr>
+                <div class="col-lg-4" style="padding-bottom: 5%;">
+                    <img src="<?php echo base_url('assets/buletin/BULETIN_MARET2018-01.jpg'); ?>" width="100%">
+                </div>
+                <div class="col-lg-8" style="padding-bottom: 5%;">
+                    <p>
+                        salah satu bentuk pelayanan informasi kepada masyarakat Provinsi Bali pada umumnya dan kota Denpasar khususnya mengenai kondisi dinamika geofisika, meteorologi, klimatologi dan kualitas udara.
+                    </p>
+                    <p>
+                        Buletin ini berisi tentang pengetahuan dan ulasan gempabumi, percepatan tanah, kelistrikan udara Pulau Bali, dinamika iklim Kota Denpasar, almanak tanda waktu kota Denpasar dan prakiraan musim hujan provinsi Bali. 
+                    </p>
+                    <p>
+                        Hasilnya disampaikan dalam bentuk informasi, tabulasi, diagram, peta dan data yang sifatnya saling melengkapi.
+                    </p>
+                    <hr>
+                    <div class="form-group" style="padding-left: 85%">
+                        <button type="submit" class="btn btn-primary">
+                            Unduh
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="padding-left: 2%; padding-right: 2%; margin-top: 1%">
+                <ul class="pagination" style="box-shadow: 1px 1px 3px grey;">
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                </ul>
         </div>
     </div>
     
-    <div class="col-lg-4" style="padding-top: 25px; padding-bottom: 100px">
-        <ul class="nav nav-tabs" style="margin-top:25px">
+    <div class="col-lg-4" style="padding-bottom: 100px">
+        <ul class="nav nav-tabs" style="margin-top: 3%">
             <li class="active">
                 <a data-toggle="tab" href="#Gempa" style="font-size: 12px; font-weight: bold">Gempabumi M ≥ 5.0</a>
             </li>
