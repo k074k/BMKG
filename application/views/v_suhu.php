4db6c8cb1ee8c20c3fff8e43cfaa64be5a1fe465
Yoga <yogasandyana92@gmail.com>
2018-04-11 08:42:24 +0800

    Revisi Grafik

--- application/views/v_suhu.php
+++ application/views/v_suhu.php
@@ -31,14 +31,14 @@
         <script type="text/javascript">
             Highcharts.chart('grafik', {
                 title: {
-                    text: 'Solar Employment Growth by Sector, 2010-2016'
+                    text: 'GRAFIK PERBANDINGAN SUHU RATA-RATA TAHUN - TERHADAP RATA-RATANYA'
                 },
                 subtitle: {
-                    text: 'Source: thesolarfoundation.com'
+                    text: 'Stasiun Geofisika Sanglah'
                 },
                 yAxis: {
                     title: {
-                        text: 'Number of Employees'
+                        text: 'Suhu Udara (C)'
                     }
                 },
                 legend: {
@@ -51,25 +51,16 @@
                         label: {
                             connectorAllowed: false
                         },
-                        pointStart: 2010
+                        pointStart: 2015
                     }
                 },
                 
                 series: [{
-                    name: 'Installation',
+                    name: 'Rata-Rata',
                     data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
                 }, {
-                    name: 'Manufacturing',
+                    name: 'Tahun',
                     data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-                }, {
-                    name: 'Sales & Distribution',
-                    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-                }, {
-                    name: 'Project Development',
-                    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-                }, {
-                    name: 'Other',
-                    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
                 }],
                 
                 responsive: {
